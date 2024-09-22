a4ace80e9a102eddec3b4041ef0b9587a88cf9c7
dedestem <78974474+dedestem@users.noreply.github.com>
2024-09-22 12:19:17 +0200

    Please Remove this future self

--- DNBot/Stop.sh
+++ DNBot/Stop.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# Delete the container
-echo "Deleting container..."
-docker-compose down -v
-
-# Optionally, remove any unused Docker resources
-echo "Removing unused Docker resources..."
-docker system prune -f
+echo "This script needs to be deleted!"
