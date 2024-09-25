00fd5c2670cc47ac322fda172d9deafada6ca32b
David Bos <david.bos@outlook.com>
2024-09-25 15:32:58 +0200

    Test1

---
+++ Web1/Start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Hello"
+echo "Checking if ROOT Exists"
+if [ -d "ROOT" ]; then
+  rm -rf ROOT
+  echo "Old ROOT deleted"
+fi
+mkdir ROOT
+
+echo "Downloading newest Web1 version"
+cd ROOT
+git clone https://github.com/dedestem/Web1 .
+cd ..
