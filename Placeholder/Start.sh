c3a3bc57d120c35996c44d263c11a7090ba49b04
dedestem <david.bos@outlook.com>
2024-10-29 13:10:15 +0100

    Placeholder

---
+++ Placeholder/Start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Hello"
+screen -S Placeholder -X quit
+
+echo "Checking if StateAPI Exists"
+if [ -d "Placeholder" ]; then
+  rm -rf Placeholder
+  echo "Old Placeholder deleted"
+fi
+
+echo "Downloading newest Placeholder version"
+git clone https://github.com/dedestem/Placeholder/
+
+cd Placeholder
+cd back-end
+echo "Installing NPM Dependencies"
+npm install
+
+echo "Bye. Say hello to placeholder.js"
+screen -S Placeholder -X quit
+screen -dmS Placeholder node placeholder.js
